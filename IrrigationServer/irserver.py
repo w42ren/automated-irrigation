025fc42e46db1f16340940a0dd6438fa685b2e6a
w42ren <warren.earle@gmail.com>
2021-04-09 15:35:04 +0100

    version 2

--- IrrigationServer/irserver.py
+++ IrrigationServer/irserver.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template,make_response
+from flask import Flask, render_template,make_response,request
 import requests
+from requests.compat import urljoin
 requests.packages.urllib3.disable_warnings()
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    btn1 = True
+    return render_template('index.html', btn=False)
 
 @app.route('/foo', methods=['GET', 'POST'])
 
@@ -34,5 +36,25 @@ def bar(x=None, y=None):
                 200,
                 )
     pass
+    
+@app.route('/toggle_pin_4', methods=['GET', 'POST'])
+def toggle_pin_4 ():
+    btn1 = True
+    if request.method == 'POST':
+        if request.form['pin4'] == 'on':
+            btn1 = False
+        elif request.form['pin4'] == 'off':
+            btn1 = True
+    base_url = "https://192.168.0.5/arduino/digital/"
+    api_url = urljoin(base_url,'4')
+    headers = { "Accept": "text/html",
+            "Content-type":"text/html"
+          }
+    resp = requests.get(api_url, headers=headers, verify=False)
+
+    return render_template('index.html', btn1 = btn1)
+    pass
+
 
 app.run(host='0.0.0.0', port=80)
+
